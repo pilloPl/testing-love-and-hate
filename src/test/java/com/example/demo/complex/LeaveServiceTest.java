11f7881ce518d71d9a7d8143137f4aebc93d6af7
Jakub Pilimon <jakub.pilimon@gmail.com>
2020-02-22 17:32:12 +0100

    start task 1

--- src/test/java/com/example/demo/complex/LeaveServiceTest.java
+++ src/test/java/com/example/demo/complex/LeaveServiceTest.java
@@ -6,10 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class LeaveServiceTest {
